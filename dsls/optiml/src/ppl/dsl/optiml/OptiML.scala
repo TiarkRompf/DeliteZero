95b2b70c21735a9fba060507298aeaace820a3a7
Tiark Rompf <tiark.rompf@epfl.ch>
2013-01-12 01:52:25 +0100

    optiml fixes

--- dsls/optiml/src/ppl/dsl/optiml/OptiML.scala
+++ dsls/optiml/src/ppl/dsl/optiml/OptiML.scala
@@ -9,13 +9,48 @@ object application {
   trait ApplicationOps
 }
 
+object datastruct {
+  object scala
+}
+
+
+
 trait OptiML extends DeliteApplication { this: OptiMLApplication =>
 
-  val Rect: RectCompanion = ???
-  abstract class RectCompanion {
-    def apply(x:Int,y:Int,w:Int,h:Int): Rect
+  object Rect {
+    def apply(x:Int,y:Int,w:Int,h:Int): Rect = ???
   }
   abstract class Rect
+
+
+  object Graph {
+    def apply[V,E](): Graph[V,E] = ???
+  }
+  abstract class Graph[V,E] {
+    def addVertex(v: Vertex[V,E])
+    def addEdge(ab: Edge[V,E], a: Vertex[V,E], b: Vertex[V,E])
+    def freeze()
+    def vertices: Iterable[Vertex[V,E]]
+    def edges: Iterable[Edge[V,E]]
+  }
+
+
+  case class Vertex[V,E](g: Graph[V,E], val data: vertexData) {
+    def edges: Iterable[Edge[V,E]] = ???
+  }
+  case class Edge[V,E](g: Graph[V,E], inData: edgeData, outData: edgeData, in: Vertex[V,E], out: Vertex[V,E])
+
+  case class vertexData(name: String, x: Int)
+  case class edgeData(name: String)
+
+
+  def UnsupervisedTrainingSet[A](x: Matrix[A]) = x
+  type UnsupervisedTrainingSet[A] = Matrix[A]
+
+  implicit class TrainingSetOps[A](x: UnsupervisedTrainingSet[A]) {
+    def numSamples: Int = ???
+    def numFeatures: Int = ???
+  }
 
 }
 
